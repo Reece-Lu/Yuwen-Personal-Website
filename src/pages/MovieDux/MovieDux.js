efa265c9e8a0ccf38d27c8dccfbfde55e2abbde2
Reece_Lu <53824755+Reece-Lu@users.noreply.github.com>
2026-02-08 14:03:01 +0800

    :bug: bugfix background colour

--- src/pages/MovieDux/MovieDux.js
+++ src/pages/MovieDux/MovieDux.js
@@ -1,11 +1,13 @@
 import React from "react";
+import MoviesGrid from "./MoviesGrid";
 
 function MovieDux() {
   return (
-    <div>
+    <div className="moviedux-page">
       <h1>MovieDux</h1>
       <p>MovieDux is a movie recommendation system that uses Redux for state management.</p>
       <p>It allows users to search for movies, view details, and add them to their watchlist.</p>
+        <MoviesGrid/>
     </div>
   );
 }
--- src/pages/MovieDux/MoviesGrid.js
+++ src/pages/MovieDux/MoviesGrid.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../MovieDux/MovieDux.styles.css';
 
 export default function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("movies.json").then((res) => res.json()).then((data) => setMovies(data));
+  }, []);
+
   return (
-    <div>Hello world</div>
+    <div className='movies-grid'>
+      {
+        movies.map(movie=>(
+            <div key={movie.id} className='movies-card'>
+              <img src={`images/movieduxImages/${movie.image}`} alt={movie.title} className='movie-image' />
+              <div className='movies-card-info'>
+                <h3 className='movie-card-title'>{movie.title}</h3>
+                <p className='movie-card-genre'>{movie.genre}</p>
+                <p className='movie-card-rating'>{movie.rating}</p>
+              </div>
+            </div>
+        ))
+      }
+    </div>
+
   );
 }
 
